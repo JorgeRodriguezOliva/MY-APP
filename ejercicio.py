eb69e7da7700ef2051a77e7c99bdf47feebaf688
JorgeRodriguezOliva <jrodriguez.oliva@gmail.com>
2024-04-18 23:33:05 -0400

    subir el archivo index.html

---
+++ ejercicio.py
@@ -0,0 +1,12 @@
+
+
+#* GUARDAMOS UN MENSAJE EN UNA VARIABLE
+
+saludo = "Bienvenidos al hola Mundo!!!"
+
+#Imprimir ese mensaje por consola
+
+print(saludo)
+
+#* ejecutar con el comamdo python  ejercicio.py
+
